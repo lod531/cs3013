623d6457c0bf443c175b11b80273290513c4693c
Andy <buinovs@tcd.ie>
2016-03-12 19:40:54 +0000

    Added minutes for week 8, fixed threads/posts omission, removed best answer

--- databaseSource/tableDefinitions.sql
+++ databaseSource/tableDefinitions.sql
@@ -2,6 +2,7 @@ CREATE TABLE courseYear
 (
  	id int NOT NULL,
 	name varchar(255) NOT NULL,
+	subtitle varchar(255) NOT NULL,
 	numericalYear int NOT NULL,
 	PRIMARY KEY (id),
 	UNIQUE (numericalYear)
@@ -11,6 +12,7 @@ CREATE TABLE yearModule
 (
  	/* name here is expected to be formatted as follows: "cs3013 Such and such" */
  	name varchar(255) NOT NULL,
+	subtitle varchar(255) NOT NULL,
 	courseYearID int NOT NULL,
 	PRIMARY KEY (name),
 	FOREIGN KEY (courseYearID) REFERENCES courseYear(id)
@@ -25,33 +27,33 @@ CREATE TABLE user
 	PRIMARY KEY (username)
 );	
 
-CREATE TABLE post
-(
- 	id int NOT NULL,
-	dateOfCreation DATETIME NOT NULL,
-	lastEdited DATETIME NOT NULL,
-	creatorID varchar(32) NOT NULL,
-	content varchar(64000) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (creatorID) REFERENCES user(username)
-);
-
 CREATE TABLE threads
 (
  	id int NOT NULL,
 	dateOfCreation DATETIME NOT NULL,
 	title varchar(255) NOT NULL,
 	parentModuleID varchar(255) NOT NULL,
-	featuredAnswerPost int NOT NULL,
 	creatorID varchar(32) NOT NULL,
 	threadText varchar(64000) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (parentModuleID) REFERENCES yearModule(name),
-	FOREIGN KEY (featuredAnswerPost) REFERENCES post(id),
 	FOREIGN KEY (creatorID) REFERENCES user(username)
 
 );
 
+
+CREATE TABLE post
+(
+ 	id int NOT NULL,
+	dateOfCreation DATETIME NOT NULL,
+	lastEdited DATETIME NOT NULL,
+	creatorID varchar(32) NOT NULL,
+	threadParentID int NOT NULL,
+	content varchar(64000) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (creatorID) REFERENCES user(username),
+	FOREIGN KEY (threadParentID) REFERENCES threads(id)
+);
 CREATE TABLE moderators
 (
  	userID varchar(32) NOT NULL,
